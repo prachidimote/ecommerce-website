5d0d80555c1ea8039e393b2da4df7797a7ac2da5
prachidimote <81797753+prachidimote@users.noreply.github.com>
2023-11-23 19:36:30 +0530

    code refractored

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,34 +1,52 @@
 import { useState, useEffect} from 'react';
 import axios from 'axios';
 import './product.scss';
+import FormatPrice from '../helpers/FormatPrice';
+
 
 const Products = () => {
     const [products, setProducts] = useState([]);
     const addToCart = (product) => {
-      // console.log(event.target);
       console.log('product', product);
     }
 
     useEffect(() => {
      axios.get('https://fakestoreapi.com/products').then( res => {
         console.log(res);
-        //update state with fetched data
         setProducts(res.data);
      })
     },[])
+    
   return (
     <div className='productContainer'>
-        <ul>
+        {/* <ul>
             {products.map((product) =>( <li key={product.id}>  <img src={product.image}/><br />{product.title}<br/>
                     {product.category}<br/>
                     {product.rating.rate}<br/>
-                    {product.price}<br/>
+                   <p>{FormatPrice}</p><br/>
             <div className='btn'>
             <button onClick={() => addToCart(product)}>Add to Cart</button>
             </div>
             </li>
             ))}
-        </ul>
+        </ul> */}
+
+
+       {/* code refractored => destructured data from an api */}
+        <ul>
+                {products.map(({ id, image, title, category, rating, price }) => (
+                    <li key={id}>
+                        <img src={image} alt={title} /><br />
+                        {title}<br />
+                        {category}<br />
+                        {rating.rate}<br />
+                        {<FormatPrice price={price}>{price}</FormatPrice>}
+                        <div className='btn'>
+                            <button onClick={() => addToCart({ id, title, price })}>Add to Cart</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
     </div>
   )
 }
