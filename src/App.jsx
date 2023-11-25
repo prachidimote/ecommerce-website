c4964cdbe1275a0a49767e3aab0f059e2763a9ac
prachidimote <81797753+prachidimote@users.noreply.github.com>
2023-11-25 07:05:24 +0530

    added cart functionality and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 import Products from "./components/products/Products";
 import Cart from './components/cart/Cart';
 
 const App = () => {
+  const [cart, setCart] = useState([]);
   return (
-    <div>
-      <Cart />
-      <Products />
-    </div>
+    <>
+    {/* <Products /> */}
+    <Router>
+      <Navbar cart={cart}/>
+      <Routes>
+        <Route path="/products" element={<Products cart={cart} setCart={setCart}/>} /> 
+        <Route path="/cart" element={<Cart cart={cart} setCart={setCart}/>} />
+      </Routes>
+    </Router>
+    
+    </>
   )
 }
 
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,12 +2,30 @@ import { useState, useEffect} from 'react';
 import axios from 'axios';
 import './product.scss';
 import FormatPrice from '../helpers/FormatPrice';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-
-const Products = () => {
+// eslint-disable-next-line react/prop-types
+const Products = ({cart, setCart}) => {
     const [products, setProducts] = useState([]);
+
+    //to get the cart items
     const addToCart = (product) => {
-      console.log('product', product);
+      // const obj = {
+      //   product
+      // }
+      setCart([...cart, product]);
+      console.log("cart Element :", cart);
+      toast.success('Item added in cart', {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
     }
 
     useEffect(() => {
@@ -18,7 +36,21 @@ const Products = () => {
     },[])
     
   return (
+    <>
+    <ToastContainer
+position="top-right"
+autoClose={1500}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="dark"
+/>
     <div className='productContainer'>
+      {/* <img style={{ cursor: "pointer" }} src='cart.png'/> */}
         {/* <ul>
             {products.map((product) =>( <li key={product.id}>  <img src={product.image}/><br />{product.title}<br/>
                     {product.category}<br/>
@@ -34,20 +66,22 @@ const Products = () => {
 
        {/* code refractored => destructured data from an api */}
         <ul>
-                {products.map(({ id, image, title, category, rating, price }) => (
+                {products.map(({ id, title, price, description, image, rating  }) => (
                     <li key={id}>
-                        <img src={image} alt={title} /><br />
+                        <img style={{ height: "60px"}} src={image} alt='' /><br />
                         {title}<br />
-                        {category}<br />
+                        {description}<br/>
                         {rating.rate}<br />
                         {<FormatPrice price={price}>{price}</FormatPrice>}
                         <div className='btn'>
-                            <button onClick={() => addToCart({ id, title, price })}>Add to Cart</button>
+                        <button onClick={() => addToCart({ id, image, title, description, price, rating })} type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Add to Cart</button>
+                         
                         </div>
                     </li>
                 ))}
             </ul>
     </div>
+    </>
   )
 }
 
