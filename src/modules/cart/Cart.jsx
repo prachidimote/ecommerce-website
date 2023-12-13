1ec4a9ff7615180cbe20c98e7ddb1230972d4ae4
prachidimote <81797753+prachidimote@users.noreply.github.com>
2023-12-13 22:25:39 +0530

    quantity updated on button in cart

--- src/modules/cart/Cart.jsx
+++ src/modules/cart/Cart.jsx
@@ -1,9 +1,44 @@
 /* eslint-disable react/no-unknown-property */
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import FormatPrice from '../../components/formatprice/FormatPrice';
 const Cart = () => {
-  const cart = JSON.parse(localStorage.getItem('cart')) || []
-  if(cart.length) <div>Cart is Empty</div>
+  const carts = JSON.parse(localStorage.getItem('cart')) || []
+  const navigate = useNavigate();
+  const handleDec = (id) => {
+    const updatedCart = carts.map(item => {
+      if(item.id === id){
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    localStorage.setItem('cart', JSON.stringify(updatedCart))
+    navigate('/cart')
+  }
+
+  const handleInc = (id) => {
+    const updatedCart = carts.map(item => {
+      if(item.id === id){
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+    navigate('/cart');
+  }
+
+  const removeProduct = (id) => {
+    const updatedCart = carts.filter(item => item.id !== id)
+    localStorage.setItem('cart', JSON.stringify(updatedCart))
+    navigate('/cart')
+  }
+
+  if(!carts.length) <div>Cart is Empty</div>
   return (
     <div className="bg-gray-100">
   <div className="container mx-auto mt-10">
@@ -11,7 +46,7 @@ const Cart = () => {
       <div className="w-3/4 bg-white px-10 py-10">
         <div className="flex justify-between border-b pb-8">
           <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-          <h2 className="font-semibold text-2xl">{cart?.length}Items</h2>
+          <h2 className="font-semibold text-2xl">{carts?.length}Items</h2>
         </div>
         <div className="flex mt-10 mb-5">
           <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
@@ -20,7 +55,7 @@ const Cart = () => {
           <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Total</h3>
         </div>
         {
-          cart?.map(cart => {
+          carts?.map(cart => {
             return(
               <div key = {cart} className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
               <div className="flex w-2/5"> 
@@ -30,16 +65,16 @@ const Cart = () => {
                 <div className="flex flex-col justify-between ml-4 flex-grow">
                   <span className="font-bold text-sm">{cart?.title}</span>
                   <span className="text-red-500 text-xs">{cart?.category}</span>
-                  <div className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</div>
+                  <div onClick={() => removeProduct(cart?.id)} className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</div>
                 </div>
               </div>
               <div className="flex justify-center w-1/5">
-                <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+                <svg onClick={() => handleDec(cart?.id)} className="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
                 </svg>
     
-                <input className="mx-2 border text-center w-8" type="text" value="1" />
+                <input className="mx-2 border text-center w-8" type="text" value={cart?.quantity} />
     
-                <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+                <svg onClick={() => handleInc(cart?.id)} className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
                   <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
                 </svg>
               </div>
@@ -65,7 +100,7 @@ const Cart = () => {
       <div id="summary" className="w-1/4 px-8 py-10">
         <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
         <div className="flex justify-between mt-10 mb-5">
-          <span className="font-semibold text-sm uppercase">Items {cart?.length}</span>
+          <span className="font-semibold text-sm uppercase">Items {carts?.length}</span>
           <span className="font-semibold text-sm">590$</span>
         </div>
         <div>
